bb95e5d434dea65a036d1dbe001f5f4795598cea
김정민/협업서비스개발블루팀/ND <jeongmin@nhndooray.com>
2022-07-25 08:53:09 +0900

    update blog at 2022년 7월 25일 월요일 08시 53분 08초 KST

--- remote_workspace_init.sh
+++ remote_workspace_init.sh
@@ -5,3 +5,16 @@ echo -e "\033[0;32mInitializing remote workspcae...\033[0m"
 # setting submodule
 git submodule init
 git submodule update
+
+# update submoudle
+# move blog/public
+
+echo "update - blog/public"
+cd public
+git pull origin main
+
+# move blog
+
+echo "update - blog"
+cd ../
+git pull origin master
